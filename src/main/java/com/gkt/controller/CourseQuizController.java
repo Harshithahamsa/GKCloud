4bdb6b58cfc66f62b0c3b149134c0d76f92e52b8
Harshithahamsa <hvharshitha97@gmail.com>
2024-06-19 15:59:20 +0530

    19-06-2024

--- src/main/java/com/gkt/controller/CourseQuizController.java
+++ src/main/java/com/gkt/controller/CourseQuizController.java
@@ -1,32 +1,33 @@
-package com.gkt.controller;
+// package com.gkt.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.gkt.repository.CourseQuizRepository;
-import com.gkt.service.CourseQuizService;
+// import com.gkt.repository.CourseQuizRepository;
+// import com.gkt.service.CourseQuizService;
 
-import net.sf.json.JSONArray;
+// import net.sf.json.JSONArray;
 
 
-@RestController
-@RequestMapping("quiz")
-@CrossOrigin(origins = "*")
-//@CrossOrigin(origins = { "*" })
-public class CourseQuizController {
+// @RestController
+// @RequestMapping("quiz")
+// @CrossOrigin(origins = "*")
+// //@CrossOrigin(origins = { "*" })
+// public class CourseQuizController {
 	
-	@Autowired
-	private CourseQuizRepository coursequizrepo;
+// 	@Autowired
+// 	private CourseQuizRepository coursequizrepo;
 
-	@Autowired
-	private CourseQuizService cousequizservice;
+// 	@Autowired
+// 	private CourseQuizService cousequizservice;
 
-	@GetMapping("/getQuizQuestions")
-	public JSONArray getCoursequizQuestions() {
-        return cousequizservice.getquizQuestions();
-    }
-
-}
+// 	@GetMapping("/getQuizQuestions")
+// 	public JSONArray getCoursequizQuestions() {
+//         return cousequizservice.getquizQuestions();
+//     }
+// Harshitha
+// }
+//
